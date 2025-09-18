fbe6b5c53c80e152129fa45348ec7ed3f5835dad
Leo <95532869+MorePeanuts@users.noreply.github.com>
2025-09-18 14:02:27 +0800

    Fixed a bug in the modify screenshot path script

--- libs/oslm-crawler/scripts/modify_screenshot_path.py
+++ libs/oslm-crawler/scripts/modify_screenshot_path.py
@@ -8,8 +8,11 @@ def main(ss_path):
         data = []
         with jsonlines.open(file, 'r') as f:
             for item in f:
-                if item['img_path'] is not None:
-                    item['img_path'] = ss_path / item['img_path'].split('screenshots')[-1]
+                if item.get('img_path') is not None:
+                    if ss_path:
+                        item['img_path'] = str(ss_path / item['img_path'].split('screenshots')[-1])
+                    else:
+                        item['img_path'] = None
                 data.append(item)
         with jsonlines.open(file, 'w') as f:
             f.write_all(data)
@@ -18,9 +21,12 @@ def main(ss_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--src", type=str, help='`screenshots` dir path, for example: /root/oslm-crawler/screenshots')
+    parser.add_argument("--null", action="store_true", help='Set all img_path fields to null')
     args = parser.parse_args()
     
-    if args.src:
+    if args.null:
+        screenshot_path = None
+    elif args.src:
         screenshot_path = Path(args.src)
     else:
         screenshot_path = Path(__file__).parents[2] / 'screenshots'
